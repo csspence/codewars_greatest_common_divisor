5d50f4ec8f017bfefd58139782fec494d618839d
Crew Spence <csspence123@gmail.com>
2021-05-15 05:20:15 -0500

    Forgot to commit this solution yesterday.

--- codewars_greatest_common_divisor.js
+++ codewars_greatest_common_divisor.js
@@ -4,6 +4,20 @@ Find the greatest common divisor of two positive integers. The integers can be l
 The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 */
 
-function mygcd(x,y){
-  //your code here
+const mygcd = (x,y) => {
+  let larger = y;
+  let smaller = x
+  let gcd = 1;
+  if(x > y) {
+    larger = x;
+    smaller = y;
+  }
+  for(let i = smaller; i > 1; i--) {
+    if(x % i === 0 && y % i === 0) {
+      gcd = i;
+      return i;
+    }
+  }
+
+  return gcd;
 }
